93949d1efe404407a2306017ad960da807ceed81
vinicius.A <vinicius.araruna18@gmail.com>
2026-04-11 12:41:36 -0300

    fix: corrige EN para PT + preparar grid inicial com parede e nada visitado

--- src/algorithms/maze_generator.py
+++ src/algorithms/maze_generator.py
@@ -2,11 +2,17 @@ import random
 
 def generate_maze(grid):
     """Usa DFS (Backtracker) pulando de 2 em 2 células para gerar o labirinto."""
-    start_cell = grid.get_cell(1,1)
+    for row in grid.celulas:
+        for cell in row:
+            cell.is_parede = True
+            cell.is_visitada = False
+    
+    start_cell = grid.get_cell(1, 1)
     if not start_cell:
         return
     
-    start_cell.is_wall = False
+    start_cell.is_parede = False
+    start_cell.is_visitada = True
     stack = [start_cell]
 
     while stack:
@@ -14,29 +20,30 @@ def generate_maze(grid):
         neighbors = []
 
         directions = [
-            (-2,0),
-            (0,2),
-            (2,0),
-            (0,-2)
+            (-2, 0),
+            (0, 2),
+            (2, 0),
+            (0, -2)
         ]
 
         for dr, dc in directions:
             nr, nc = current.row + dr, current.col + dc
-            neighbor = grid.get_cell(nr,nc)
+            neighbor = grid.get_cell(nr, nc)
 
-            if neighbor and neighbor.is_wall:
-                if 0 < nr < grid.rows - 1 and 0 < nc < grid.cols - 1:
-                    neighbor.append((neighbor,dr,dc))
+            if neighbor and 0 < nr < grid.rows - 1 and 0 < nc < grid.cols - 1 and neighbor.is_parede and not neighbor.is_visitado:
+                neighbors.append((neighbor, dr, dc))
 
         if neighbors:
-            next_cell, dr , dc = random.choice(neighbors)
+            next_cell, dr, dc = random.choice(neighbors)
 
-            mid_r = current.row + (dr//2)
-            mid_c = current.col + (dc//2)
-            grid.get_cell(mid_r,mid_c).is_wall = False
+            mid_r = current.row + (dr // 2)
+            mid_c = current.col + (dc // 2)
+            grid.get_cell(mid_r, mid_c).is_parede = False
 
-            next_cell.is_wall = False
+            next_cell.is_parede = False
+            next_cell.is_visitado = True
 
             stack.append(next_cell)
         else:
             stack.pop()
+
